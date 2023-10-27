75191c9ac0182dc69f0b11deb04f0169b27a8eaf
AlpacaSundae <jaicobschott@hotmail.com>
2023-10-27 13:12:35 +0800

    change to dict of lists for subdir discrimination

--- studdybudy/SoundRandomiser.py
+++ studdybudy/SoundRandomiser.py
@@ -10,34 +10,38 @@ class SoundRandomiserError(Exception):
 # Sound effects are loaded in from a directory (and its subs) and played back with pygame
 class SoundRandomiser():
     def __init__(self, sfx_dir = "./media/randomiser", prob = 0.0000001):
-        self.sfxLoad(sfx_dir)
+        self.root_dir = os.path.normpath(sfx_dir)
+        self.sfxLoadDir(self.root_dir)
         self.setProbability(prob)
         self.stop = False
 
     # loads any wav files in the directory provided
     # checks subfolders to allow the user to sort samples
-    def sfxLoad(self, sfx_dir, init = True):
-        try:
+    # each subdirectory is stored as a new list within the sfx_store dict
+    def sfxLoadDir(self, sfx_dir, init = True):
             if (not os.path.isdir(sfx_dir)):
                 raise SoundRandomiserError(f"\'{sfx_dir}\' is not a valid directory")
-
-            # this allows reruns for adding dirs without clearing previous loading
-            if (init):
-                self.sfx_store = []
             
+            if (init):
+                self.sfx_store = {}
+                self.sfx_alt_store = {}
+
             for root, dirs, files in os.walk(sfx_dir):
+                # for each subdirectory with files we will create a list of pygame sounds
+                temp_store = []
                 for filename in files:
-                    if filename.endswith(".wav"):
-                        self.sfx_store.append(pygame.mixer.Sound(os.path.join(root, filename)))
+                    if filename.lower().endswith(".wav"):
+                        try:
+                            temp_store.append(pygame.mixer.Sound(os.path.join(root, filename)))
+                        except pygame.error as e:
+                            print(f"Error loading file: {os.path.join(root, filename)}") # print info for now,, might change to raise error later
+                
+                # and we add it to the sfx dict only if a sound file was found and loaded
+                if (len(temp_store) != 0):
+                    self.sfx_store[root] = temp_store
 
-            # len 0 means no wav files ever load
-            # only important for functionality on the first dir load
             if (len(self.sfx_store) == 0):
-                raise SoundRandomiserError(f"\'{sfx_dir}\' contains no .wav files")
-
-        # must run pygame.init() beforehand
-        except pygame.error as e:
-            raise SoundRandomiserError("pygame not initialised")
+                raise SoundRandomiserError(f"no .wav files could be loaded from: \'{sfx_dir}\'")
 
     def setProbability(self, prob):
         if (prob >= 0 and prob <= 1):
@@ -46,7 +50,7 @@ class SoundRandomiser():
             raise SoundRandomiserError(f"Probablitiy must be in range [0.0, 1.0], given: {prob}")
 
     def playRandom(self):
-        random.choice(self.sfx_store).play()
+        random.choice(random.choice(list(self.sfx_store.values()))).play()
 
     def roll(self):
         result = random.random()
